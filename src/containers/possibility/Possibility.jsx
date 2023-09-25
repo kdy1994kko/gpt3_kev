4115859b1d1030d3eb8c31717d2ba38685f5433a
Kevvan D. Young <101077732+kdy1994kko@users.noreply.github.com>
2023-09-25 08:59:25 -0600

    Add files via upload

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
+import possibilityImage from '../../assets/possibility.png';
 import './possibility.css';
 
 const Possibility = () => {
   return (
-    <div>
-        <h1>Possibility</h1> 
+    <div className="gpt3__possibility section__padding" id="possibility"> 
+      <div className="gpt3__possibility-image">
+        <img src={possibilityImage} alt="possibility" />
+      </div>
+      <div className="gpt3__possibility-content">
+        <h4>Request Early Access to Get Started</h4>
+        <h1 className="gradient__text">Unleash the Profound Depths of Your Imagination through Advanced AI</h1>
+        <p>Engaging in interactive conversations, using it as a brainstorming tool, experimenting with AI-generated content, and embracing collaboration to explore uncharted creative territories.</p>
+        <h4>Request Early Access to Get Started</h4>
+      </div>
     </div>
   ) 
-}
-
+} 
+ 
 export default Possibility
