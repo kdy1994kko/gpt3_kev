912739e363193f24e013aa391a19ed8852ed7663
Kevvan D. Young <101077732+kdy1994kko@users.noreply.github.com>
2023-09-26 06:48:18 -0600

    Add files via upload

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React from 'react';
 import './footer.css';
 
-const Footer = () => {
-  return (
-    <div>
-        <h1>Footer</h1> 
+const Footer = () => (
+  <div className="gpt3__footer section__padding">
+    <div className="gpt3__footer-heading">
+    <a href="https://www.biblegateway.com/passage/?search=john+3%3A16&version=NIV" target="_blank" rel="noreferrer">
+      <h1 className="gradient__text">"For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life."</h1>
+      <h2 className="gradient__text">~ John 3:16</h2>
+    </a>
     </div>
-  ) 
-}
+  </div>
+);
 
-export default Footer  
+export default Footer;
+
+  
